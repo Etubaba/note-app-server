1c78c09c728ea77f95aefc51e891c26546c71e23
Etumnu Prosper <johnikechukwu94@gmail.com>
2023-01-28 06:20:31 +0100

    Fixed cors error

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: ['POST', 'PUT', 'DELETE', 'GET', 'PATCH'],
+    optionsSuccessStatus: 200,
+  });
+
   await app.listen(process.env.PORT || 8080, () => {
     console.log(`Listening at port ${process.env.PORT || 8080}`);
   });
